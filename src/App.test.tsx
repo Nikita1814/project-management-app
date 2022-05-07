9b652812fe0067daebea4ab25880473ab0d3ed3a
Evgeniy Dainovich <evg.dainovich@gmail.com>
2022-05-07 20:31:52 +0300

    fix: test if testing works

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,6 +9,6 @@ test('renders learn react link', () => {
       <App />
     </MemoryRouter>
   );
-  const linkElement = screen.getByText('Todo-App');
+  const linkElement = screen.getByText('Todo-666App');
   expect(linkElement).toBeInTheDocument();
 });
